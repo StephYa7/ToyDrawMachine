1396cfd112c20e6d9785b6e2249decc316c6f07c
StephYa7 <1000p@mail.ru>
2023-08-16 01:20:30 +0700

    +class Toy +Start Main

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,37 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
+
+    public static List<Toy> toys = new ArrayList<Toy>();
+    public static List<Toy> winningToysWaiting = new ArrayList<Toy>();
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Toy a = new Toy(01, "aa", 12, 1);
+        Toy b = new Toy(02, "bb", 3, 5);
+        Toy c = new Toy(03, "cc", 31, 3.5);
+        Toy d = new Toy(04, "dd", 2, 10);
+
+        toys.add(a);
+        toys.add(b);
+        toys.add(c);
+        toys.add(d);
+        chooseWinningToy();
+
+
     }
+
+
+    public static Toy chooseWinningToy() {
+        double totalWeight = 0;
+        for (Toy toy : toys) {
+            totalWeight += (toy.getWinWeight() * toy.getCount());
+        }
+        System.out.println(totalWeight);
+
+
+        return null;
+    }
+
+
 }
--- src/Toy.java
+++ src/Toy.java
@@ -1,2 +1,40 @@
 public class Toy {
+    private int id;
+    private String name;
+    private int count;
+    private double winWeight;
+
+    public Toy(int id, String name, int count, double winWeight) {
+        this.id = id;
+        this.name = name;
+        this.count = count;
+        this.winWeight = winWeight;
+    }
+
+    public void setCount(int count) {
+        this.count = count;
+    }
+
+    public void setWinWeight(double winWeight) {
+        this.winWeight = winWeight;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public double getWinWeight() {
+        return winWeight;
+    }
+    public void addMoreToys(int count){
+        this.setCount(this.getCount()+count);
+    }
 }
